9482e575a307a46cccbf7744726d42eee4acf58a
Hyeok Min Kwon <khmzxc@naver.com>
2020-04-23 18:15:42 +0900

    Fix lint

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ import cors from "@koa/cors";
 const { PORT, MONGO_URI } = process.env;
 
 Mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((e) => {
-    console.error(e);
-  });
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch(e => {
+		console.error(e);
+	});
 
 const app = new Koa();
 const router = new Router();
@@ -28,5 +28,5 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
 app.listen(port, () => {
-  console.log("Listening to port %d", port);
+	console.log("Listening to port %d", port);
 });
