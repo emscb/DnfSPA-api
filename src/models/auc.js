fdd8b685a686de674c29ce3f1ff0dc2e2bb6a612
Hyeok Min Kwon <khmzxc@naver.com>
2020-05-07 15:16:38 +0900

    Disable version key

--- src/models/auc.js
+++ src/models/auc.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const AucSchema = new Schema({
-	date: Date,
-	itemName: String,
-	itemId: String,
-	avgPrice: Number,
-});
+const AucSchema = new Schema(
+	{
+		date: Date,
+		itemName: String,
+		itemId: String,
+		avgPrice: Number,
+	},
+	{ versionKey: false }
+);
 
 const Auc = mongoose.model("Auc", AucSchema);
 
